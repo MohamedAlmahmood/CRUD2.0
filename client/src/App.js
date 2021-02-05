a0a9fd375efbf03ae897d8fd7b9ea2fbba71aa3c
MohamedAlmahmood <xhamadeex@gmail.com>
2021-02-05 18:16:36 +0300

    trying to make validation work

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,40 @@
 import './App.css';
 import {useState} from "react";
+import Axios from 'axios';
 
 function App() {
   const [name, setname] = useState("");
   const [age, setage] = useState(0);
-  const [Position, setposition] = useState("");
+  const [position, setposition] = useState("");
   const [salary, setsalary] = useState(0);
 
-
+  const addEmployee = ()=>{
+    Axios.post('http://localhost:3001/create', {
+      name:name, 
+      age:age, 
+      position:position, 
+      salary:salary,
+      }
+    ).then( ()=>{console.log("success")} )
+  }
 
   return (
     <div className="App">
+
       <div className="information">
         <label>Name:</label>
         <input type="text" onChange={ (event)=> {setname(event.target.value)} }></input>
         <label>Age:</label>
-        <input type="number" onChange={ (event)=> {setage(event.targer.value)} }></input>
+        <input type="number" onChange={ (event)=> {setage(event.target.value)} }></input>
         <label>Position:</label>
         <input type="text" onChange={ (event)=> {setposition(event.target.value)} }></input>
         <label>salary:</label>
         <input type="number" onChange={ (event)=> {setsalary(event.target.value)} }></input>
-        <button>Add Employee</button>
+        <button onClick={addEmployee}>Add Employee</button>
       </div>
+      
+      <hr/>
+
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,53 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql')
+const cors = require('cors')
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: 'password',
+    database: 'hrmanagement',
+});
 
 app.listen(3001, ()=>{
     console.log("server running on port 3001")
+})
+
+app.post('/create', (req,res)=> { //req= request from front end, res= send to front end
+    const name = req.body.name;
+    const age = req.body.age;
+    const position = req.body.position;
+    const salary = req.body.salary;
+    var count=0;
+    /*
+    db.query(
+        "SELECT * FROM employeeinfo WHERE name like ? and age = ?",[name, age], function(err, value, val){
+            if(err) {
+                throw err;
+              } else {
+                count = value.length;
+                console.log(count);
+              }
+        }
+    )*/
+/*   
+console.log(count);
+    if (count != 0){*/
+    db.query(
+        "INSERT INTO employeeinfo (name, age, position, salary) VALUES (?,?,?,?)", 
+        [name, age, position, salary],
+        (err, result)=>{
+            if (err){
+                console.log(err)
+            }else{
+                res.send("values inserted")}} 
+    )
+    /*   
+}else 
+    console.log("Sorry record already exist");*/
+
 })
