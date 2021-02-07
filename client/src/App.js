bd468c713e3a81d9e53f130e6fcbca02e5e79b21
MohamedAlmahmood <xhamadeex@gmail.com>
2021-02-07 10:24:33 +0300

    update is working

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ function App() {
   const [age, setage] = useState(0);
   const [position, setposition] = useState("");
   const [salary, setsalary] = useState(0);
+  const [employeelist, setemployeelist] = useState([]);
+  const [newsalary, setnewsalary] = useState(0);
 
   const addEmployee = ()=>{
     Axios.post('http://localhost:3001/create', {
@@ -15,12 +17,33 @@ function App() {
       position:position, 
       salary:salary,
       }
-    ).then( ()=>{console.log("success")} )
+    ).then( ()=> {
+      setemployeelist([...employeelist, { //array destructuring to automatically show the new employee without refreshing
+        name:name, 
+        age:age, 
+        position:position, 
+        salary:salary,
+      }])
+    } )
+  }
+
+  const getEmployees = ()=>{
+    Axios.get('http://localhost:3001/employees').then((response)=>{//response will get whatever we get from backend
+      setemployeelist(response.data);//array of the database
+    })
+  }
+
+  const updateEmployeeSalary = (id)=>{
+    Axios.put('http://localhost:3001/update', {
+      salary: newsalary,
+      id: id
+    }).then((response)=>{
+      alert('update')
+    })
   }
 
   return (
     <div className="App">
-
       <div className="information">
         <label>Name:</label>
         <input type="text" onChange={ (event)=> {setname(event.target.value)} }></input>
@@ -32,8 +55,33 @@ function App() {
         <input type="number" onChange={ (event)=> {setsalary(event.target.value)} }></input>
         <button onClick={addEmployee}>Add Employee</button>
       </div>
-      
+
       <hr/>
+    
+      
+      <div className="employees">
+        <button onClick={getEmployees}>show all employees</button>
+        {employeelist.map((val,key)=>{
+          return( 
+            <div className="employee">
+              <div>
+                <h3>Name: {val.name}</h3>
+                <h3>Age: {val.age}</h3>
+                <h3>Position: {val.position}</h3>
+                <h3>Salary: {val.salary}</h3> 
+              </div>
+              <div>
+                <input type="number" placeholder="update salary" onChange={(event)=>{setnewsalary(event.target.value)}}></input>
+                <button onClick={()=>{
+                  updateEmployeeSalary(val.id)
+                }}>Update</button>
+              </div>
+            </div>
+            
+            )
+        })}
+      </div>
+     
 
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( {extended: false} ))
 
 const db = mysql.createConnection({
     user: 'root',
@@ -22,7 +25,7 @@ app.post('/create', (req,res)=> { //req= request from front end, res= send to fr
     const age = req.body.age;
     const position = req.body.position;
     const salary = req.body.salary;
-    var count=0;
+    //var count=0;
     /*
     db.query(
         "SELECT * FROM employeeinfo WHERE name like ? and age = ?",[name, age], function(err, value, val){
@@ -50,4 +53,28 @@ console.log(count);
 }else 
     console.log("Sorry record already exist");*/
 
+})
+
+//get the array and send it to front end
+app.get('/employees', (req, res)=>{
+    db.query( "SELECT * FROM employeeinfo", (err, result)=>{
+            if (err){
+                console.log(err)
+            }else {
+                res.send(result);
+            }
+        }
+    )
+})
+//Update the employee
+app.put('/update', (req,res)=>{
+    const id = req.body.id; //get it from front end
+    const salary = req.body.salary; //get it from front end
+    db.query("UPDATE employeeinfo SET salary=? WHERE id=?", [salary, id], (err, result)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    })
 })
